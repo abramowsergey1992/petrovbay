c04bbe744bcd728c558810772b987a6e880b0a58
Сергей Абрамов <abramowsergey1992@gmail.com>
2022-09-27 15:53:23 +0500

    front page

---
+++ src/layout/component/aos/aos.js
@@ -0,0 +1,16 @@
+$(function () {
+	$(".text p").each(function () {
+		$(this).attr("data-aos", "fade-up");
+	});
+	AOS.init({});
+	// document.addEventListener("aos:in", ({ detail }) => {
+	// 	if ($(detail).hasClass("audio-player")) {
+	// 		$(detail).find(".audio-player__play").trigger("click");
+	// 	}
+	// });
+	// document.addEventListener("aos:out", ({ detail }) => {
+	// 	if ($(detail).hasClass("audio-player")) {
+	// 		$(detail).find(".audio-player__stop").trigger("click");
+	// 	}
+	// });
+});
